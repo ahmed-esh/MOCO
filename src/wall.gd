cb6664e84269fce2b4ed9b14ce58719cf05dc36d
jontopielski <polkorame@gmail.com>
2026-09-15 17:05:17 -0400

    Remove walls

--- src/wall.gd
+++ src/wall.gd
@@ -4,10 +4,14 @@ extends Node2D
 @export var top_color : Color = Color.WHITE
 @export var height : int = 10
 
+var player_ref : Node2D = null
 
 func _ready():
 	$Line2D.hide()
 	spawn_lines()
+	var player_check = get_tree().get_first_node_in_group("Player")
+	if player_check:
+		player_ref = player_check
 
 func spawn_lines():
 	for i in range(0, height):
@@ -19,3 +23,15 @@ func spawn_lines():
 			next_line.default_color = top_color
 		next_line.show()
 	
+func _process(delta):
+	if player_ref and not Engine.is_editor_hint():
+		update_stack_direction()
+
+func update_stack_direction():
+	if !has_node("Walls"):
+		return
+	for i in range(0, $Walls.get_child_count()):
+		var camera_rotation = 0.0 if Engine.is_editor_hint() else Globals.Rotation
+		var next_sprite = $Walls.get_child(i)
+		var start_offset = Vector2.UP.rotated(camera_rotation)
+		next_sprite.position = Vector2.UP.rotated(camera_rotation) * i
