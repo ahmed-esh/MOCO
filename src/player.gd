d85c9817e20f11c820ef9106f24eac464ae7ae31
jontopielski <polkorame@gmail.com>
2026-09-15 16:33:05 -0400

    Add camera rotation

--- src/player.gd
+++ src/player.gd
@@ -8,9 +8,13 @@ func _process(delta):
 	input.x = Input.get_axis("left", "right")
 	input.y = Input.get_axis("up", "down")
 	
+	input = input.normalized().rotated(Globals.Rotation)
+	
 	position += input.normalized() * walk_speed * delta
 	
 	if Input.is_action_pressed("rotate_left"):
 		Globals.Rotation += rotation_speed * delta
+		$Camera2D.rotation = Globals.Rotation
 	if Input.is_action_pressed("rotate_right"):
 		Globals.Rotation -= rotation_speed * delta
+		$Camera2D.rotation = Globals.Rotation
--- src/stacked/stacked_sprite.gd
+++ src/stacked/stacked_sprite.gd
@@ -3,7 +3,8 @@ extends Sprite2D
 
 var player_ref : Node2D = null
 
-@onready var start_position : Vector2 = position
+@onready var start_position : Vector2 = global_position
+@onready var start_angle : float = global_position.angle()
 
 @export var stack_texture : Texture2D = null :
 	set(value):
@@ -34,28 +35,17 @@ func _process(delta):
 		return
 	if $Stack.get_child_count() == 0:
 		setup_stack()
-	if player_ref:
-		update_position_to_player()
-		update_stack_camera_angle()
+	if player_ref and not Engine.is_editor_hint():
+		update_stack_direction()
 
-func update_position_to_player():
-	if not player_ref or not find_child("Stack"):
+func update_stack_direction():
+	if !has_node("Stack"):
 		return
-	var player_position = player_ref.position
-	var distance_to_player = position.distance_to(player_position)
-	var angle_from_player_to_stack = player_position.angle_to(position)
-	var updated_angle = angle_from_player_to_stack + Globals.Rotation
-	var updated_position = player_ref.position + Vector2(cos(updated_angle), sin(updated_angle)) * distance_to_player
-	position = updated_position
-
-func update_stack_camera_angle():
-	if not player_ref or not find_child("Stack"):
-		return
-	for i in range(0, hframes):
-		var next_child = $Stack.get_child(i)
-		var next_x = cos(Globals.Rotation)
-		var next_y = sin(Globals.Rotation)
-		next_child.position = Vector2(next_x, next_y) * i
+	for i in range(0, $Stack.get_child_count()):
+		var camera_rotation = 0.0 if Engine.is_editor_hint() else Globals.Rotation
+		var next_sprite = $Stack.get_child(i)
+		var start_offset = Vector2.UP.rotated(camera_rotation)
+		next_sprite.position = Vector2.UP.rotated(camera_rotation) * i
 
 func set_stack_rotation(rot):
 	if not has_node("Stack"):
